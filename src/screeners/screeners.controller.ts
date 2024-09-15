cf8fee83c3e16e6955a1cda4514a6e1ab30e9c0c
Jim Fiorato <jfiorato@gmail.com>
2024-09-15 08:22:29 -0500

    add dummy API argument to make it RESTy

--- src/screeners/screeners.controller.ts
+++ src/screeners/screeners.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ScreenersService } from './screeners.service';
 import { SubmitScreenerDto } from './dto/submit-screener.dto';
 
@@ -6,8 +6,10 @@ import { SubmitScreenerDto } from './dto/submit-screener.dto';
 export class ScreenersController {
   constructor(private readonly screenerService: ScreenersService) {}
 
-  @Get()
-  fetch() {
+  @Get(':id')
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  findOne(@Param('id') _id: string) {
+    // Only one screener for this example
     return this.screenerService.fetch();
   }
 
